1857438c5371e43f8df9eb7348b13776d5653170
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-05-18 16:01:40 +0530

    Time: 4 ms (92.84%), Space: 59.6 MB (92.98%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,83 +1,71 @@
 class Solution {
+    int[] parent;
+    int[] rank;
+    
     public int makeConnected(int n, int[][] connections) {
-        System.out.println(n);
-        // Adjacency list
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        parent = new int[n];
+        rank = new int[n];
+        
+        
         for(int i = 0; i < n; i++){
-            graph.add(new ArrayList());
+            parent[i] = i;
+            rank[i] = 1;
         }
         
+        int red = 0;
         for(int[] row: connections){
             int u = row[0];
             int v = row[1];
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+            
+            if(find(u) == find(v)){
+                red++;
+            }else
+                union(u, v);
         }
         
-        // Calculate components
-        boolean vis[] = new boolean[n];
-        int comp = 0;
-        for(int i = 0; i < n; i++){
-            if(!vis[i]){
-                comp++;
-                dfs(i, graph, vis);
+        // REDUNDANT EDGES
+        // int totalEdges = connections.length;
+        // if (totalEdges < (n-1)) return -1;
+        // return red; 
+
+        int nodesunVisited = 0;
+        for(int i = 0; i < parent.length; i++){
+            if(find(i) == i){
+                nodesunVisited++;
             }
         }
         
-        // System.out.println(comp);
-        
-        int totalEdges = connections.length;
-        if (totalEdges < (n-1)) return -1;
-        return (comp-1);
-    }
-    
-    public void dfs(int i, ArrayList<ArrayList<Integer>> adj, boolean[] vis) {
-        // Code here
-        if(vis[i]) return;
-        
-        vis[i] = true;
-        
-        for(int j: adj.get(i)){
-            dfs(j, adj, vis);
+        if(red >= nodesunVisited - 1){
+            return nodesunVisited - 1;
         }
+        
+        return -1;
+    }
+  
+    
+    int find(int x){
+        if(parent[x] == x) return x;
+        int temp = find(parent[x]);
+        parent[x] = temp;
+        return temp;
     }
     
-//     CANT BE DONE USING UNION & PATH COMPRESSION- Different concept
-//     int[] parent;
-//     int[] rank;
-    
-//     parent = new int[n];
-//     rank = new int[n];
-
-//     int count = 0;
-//     for(int i = 0; i < n; i++){
-//         parent[i] = i;
-//         rank[i] = 1;
-//     }
-    
-//     int find(int x){
-//         if(parent[x] == x) retrun x;
-//         int temp = parent(x);
-//         parent[x] = temp;
-//         return temp;
-//     }
-    
-//     boolean union(int x, int y){
-//         int lx = find(x);
-//         int ly = find(y);
+    boolean union(int x, int y){
+        int lx = find(x);
+        int ly = find(y);
         
-//         if(lx!=ly){
-//             if(rank[lx] > rank[ly]){
-//                 parent[ly] = lx;
-//             }else if(rank[lx] < rank[ly]){
-//                 parent[lx] = ly;
-//             }else{
-//                 parent[ly] = lx;
-//                 rank[lx]++;
-//             }
-//             return true;
-//         }
+        if(lx!=ly){
+            if(rank[lx] > rank[ly]){
+                parent[ly] = lx;
+            }else if(rank[lx] < rank[ly]){
+                parent[lx] = ly;
+            }else{
+                parent[ly] = lx;
+                rank[lx]++;
+            }
+            return true;
+        }
         
-//         return false;
-//     }
+        return false;
+    }
 }
