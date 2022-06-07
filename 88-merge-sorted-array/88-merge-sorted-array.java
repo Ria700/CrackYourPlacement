12005528df42422089ff2fedbe379fb278d5f380
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-06-07 10:13:48 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (9.48%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0, j = 0, k = 0;
+        int[] ans = new int[m+n];
+        while(i < m && j < n){
+            if(nums1[i] < nums2[j]){
+                ans[k] = nums1[i];
+                i++;
+            }else if(nums1[i] >= nums2[j]){
+                ans[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while(i < m){
+            ans[k] = nums1[i];
+            i++;
+            k++;
+        }
+        
+        while(j < n){
+            ans[k] = nums2[j];
+            j++;
+            k++;
+        }
+        
+        k = 0;
+        while(k < m+n){
+            nums1[k] = ans[k];
+            k++;
+        }
+    }
+}
