08a9b8e9abeba19dc7027b4cb1cf2eb110aab7b3
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-06-08 14:00:57 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (47.22%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
@@ -1,18 +1,22 @@
 class Solution {
-    public int removePalindromeSub(String s) {
-        if(s.length() == 0) return 0;
-        if(isPalindrome(s)) return 1;
-        return 2; // only 2 char and we can remove palindromic subsequence
-    }
+//     public int removePalindromeSub(String s) {
+//         if(s.length() == 0) return 0;
+//         if(isPalindrome(s)) return 1;
+//         return 2; // only 2 char and we can remove palindromic subsequence
+//     }
     
-    public boolean isPalindrome(String s){
-        int i = 0, j = s.length()-1;
-        while(i < j){
-            if(s.charAt(i) != s.charAt(j)) return false;
-            i++;
-            j--;
-        }
+//     public boolean isPalindrome(String s){
+//         int i = 0, j = s.length()-1;
+//         while(i < j){
+//             if(s.charAt(i) != s.charAt(j)) return false;
+//             i++;
+//             j--;
+//         }
         
-        return true;
+//         return true;
+//     }
+    
+    public int removePalindromeSub(String s) {
+        return s.isEmpty() ? 0 : (s.equals(new StringBuilder(s).reverse().toString()) ? 1:2);
     }
 }
