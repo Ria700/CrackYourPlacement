f1cee3a9433cdafc3f612e277ab28e8d67570959
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-06-21 13:40:33 +0530

    Time: 493 ms (10.36%), Space: 50.2 MB (76.83%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.java
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -1,44 +1,13 @@
-class TrieNode {
-    public TrieNode[] children;
-    public int height;
-    public TrieNode() {
-        children = new TrieNode[26];
-        height = 0;
-    }
-}
-
 class Solution {
-    
     public int minimumLengthEncoding(String[] words) {
-        TrieNode root = new TrieNode();
-        for (String word: words) insert(root, word);
-        
-        Stack<TrieNode> stack = new Stack<>();
-        stack.add(root);
-        
-        int size = 0;
-        while (!stack.isEmpty()) {
-            TrieNode node = stack.pop();
-            int numOfChildren = 0;
-            for (TrieNode child: node.children) {
-                if (child == null) continue;
-                numOfChildren++;
-                stack.add(child);
+        // M-1
+        Arrays.sort(words, (a, b) -> b.length() - a.length());
+        StringBuilder encodings = new StringBuilder();
+        for(String s: words){
+            if(encodings.indexOf(s+"#") == -1){
+                encodings.append(s+"#");
             }
-            if (numOfChildren == 0) size += node.height + 1;
         }
-        
-        return size;
-    }
-    
-    private void insert(TrieNode root, String word) {
-        for (int i=word.length()-1; i>=0; i--) {
-            char c = word.charAt(i);
-            if (root.children[c - 'a'] == null) 
-                root.children[c - 'a'] = new TrieNode();
-            int prevHeight = root.height;
-            root = root.children[c - 'a'];
-            root.height = prevHeight + 1;
-        }
+        return encodings.length();
     }
 }
