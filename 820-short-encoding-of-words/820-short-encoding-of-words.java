e85ca92ae2e4fa7065dba24c76c407416651e340
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-06-21 13:52:17 +0530

    Time: 24 ms (79.27%), Space: 53.9 MB (70.73%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.java
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -1,13 +1,25 @@
 class Solution {
     public int minimumLengthEncoding(String[] words) {
-        // M-1
-        Arrays.sort(words, (a, b) -> b.length() - a.length());
-        StringBuilder encodings = new StringBuilder();
-        for(String s: words){
-            if(encodings.indexOf(s+"#") == -1){
-                encodings.append(s+"#");
-            }
-        }
-        return encodings.length();
+//         M-1: TC: O(nlogn) SC: O(Length of all words)
+//         Arrays.sort(words, (a, b) -> b.length() - a.length());
+//         StringBuilder encodings = new StringBuilder();
+//         for(String s: words){
+//             if(encodings.indexOf(s+"#") == -1){
+//                 encodings.append(s+"#");
+//             }
+//         }
+//         return encodings.length();
+        
+//      M-2: TC: O(w^2) SC: O(n)
+        HashSet<String> set = new HashSet<>(Arrays.asList(words));
+        for(String s: words)
+            for(int i = 1; i < s.length(); i++)
+                set.remove(s.substring(i)); // i to END
+        
+        int length = 0;
+        for(String word: set)
+            length += word.length() + 1;
+        
+        return length;
     }
 }
