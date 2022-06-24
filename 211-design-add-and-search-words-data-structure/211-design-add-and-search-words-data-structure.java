f608fa12cc4539e3dba866f884cc8c205c1a9fe5
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-06-24 14:52:46 +0530

    Time: 833 ms (72.70%), Space: 85.4 MB (97.83%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.java
@@ -0,0 +1,56 @@
+class TrieNode{
+    TrieNode children[];
+    boolean isTerminating;
+    
+    TrieNode(){
+        children = new TrieNode[26];
+        isTerminating = false;
+    }
+}
+class WordDictionary {
+    
+    TrieNode root;
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    public void addWord(String word) {
+        TrieNode temp = root;
+        for(char c: word.toCharArray()){
+            if(temp.children[c-'a'] == null) 
+                temp.children[c-'a'] = new TrieNode();
+            temp = temp.children[c-'a'];
+        }
+        temp.isTerminating = true;
+    }
+    
+    public boolean search(String word) {
+        return search(word, root);
+    }
+    
+    public boolean search(String word, TrieNode r) {
+        TrieNode temp = r;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(c == '.'){
+                for(TrieNode child: temp.children) {
+                    if(child!=null && search(word.substring(i+1, word.length()), child))
+                        return true;
+                }
+                return false;
+            }
+            
+            temp = temp.children[c-'a'];
+            if(temp == null) return false;
+        }
+        
+        return temp.isTerminating;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
