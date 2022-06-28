32cee556dc08f5289ecab0211ddd0980cb696784
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-06-28 17:36:06 +0530

    Time: 109 ms (75.55%), Space: 124.3 MB (51.73%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -6,40 +6,19 @@ class Solution {
         }
         
         for(int i = 0; i < n; i++){
-            int parent = manager[i];
-            if(parent == -1) continue;
-            adj.get(parent).add(i);
+            if(i != headID)
+                adj.get(manager[i]).add(i);
         }
         
-        // BFS wont work
-        // Queue<Integer> q = new LinkedList<>();
-        // q.add(headID);
-        // int level = 0, inf = 0;
-        // while(!q.isEmpty()){
-        //     int size = q.size();
-        //     inf  = 0;
-        //     while(size-- > 0){
-        //         int rem = q.remove();
-        //         inf = Math.max(inf, informTime[rem]);
-        //         for(int i: adj.get(rem)){
-        //             q.add(i);
-        //         }
-        //     }
-        //     level+=inf;
-        // }
-        // return level;
-        
-        return calCost(headID, adj, informTime);
-        
-        
+        return helper(headID, adj, informTime);
     }
     
-    public int calCost(int manager, ArrayList<ArrayList<Integer>> adj, int[] informTime){
-        int cost = 0;
-        for(int i: adj.get(manager)){
-            cost = Math.max(cost, calCost(i, adj, informTime));
+    public int helper(int i, ArrayList<ArrayList<Integer>> adj, int[] informTime){
+        int ans = 0;
+        for(int j: adj.get(i)){
+            ans = Math.max(ans, helper(j, adj, informTime));
         }
-        
-        return cost+informTime[manager];
+                           
+        return informTime[i] + ans;
     }
 }
