e307d84f3d1269e13d71e590cda3daa1a62685e4
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-07-01 15:19:49 +0530

    Time: 15 ms (41.95%), Space: 53.8 MB (70.71%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.java
+++ 934-shortest-bridge/934-shortest-bridge.java
@@ -1,57 +1,53 @@
-
 class Solution {
-    Queue<int[]> que = new LinkedList<>();
-    int[][] directions = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-    public int shortestBridge(int[][] grid) {
-        int shortestBridgeLength = Integer.MAX_VALUE;
+    public int shortestBridge(int[][] A) {
+        int m = A.length, n = A[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        Queue<int[]> q = new LinkedList<>();
         boolean found = false;
-        int l = -1, m = -1;
-        //Find a insland cordinate
-        for(int i = 0; i<grid.length; i++){
-            for(int j = 0; j<grid.length; j++){
-                if(grid[i][j] == 1){
-                    l = i;
-                    m = j;
+        // 1. dfs to find an island, mark it in `visited`
+        for (int i = 0; i < m; i++) {
+            if (found) {
+                break;
+            }
+            for (int j = 0; j < n; j++) {
+                if (A[i][j] == 1) {
+                    dfs(A, visited, q, i, j, dirs);
+                    found = true;
+                    break;
                 }
             }
         }
-        //Find all the cordinate of one island, and store it's cordinate in que
-        dfs(l,m,grid);
-        //BFS
-        while(!que.isEmpty()){
-            int[] info = que.poll();
-            int x = info[0];
-            int y = info[1];
-            int length = info[2];
-            if(grid[x][y] == 1){
-                shortestBridgeLength = Math.min(length,shortestBridgeLength);
-            }
-            //If we already visted a cordinate or the length is less than thre result
-            if(grid[x][y] < 0 || (shortestBridgeLength < Integer.MAX_VALUE && shortestBridgeLength <= length)){
-                continue;
-            }
-            if(grid[x][y] >= 0){
-                grid[x][y] = -1;
-            }
-            for(int[] direction : directions){
-                int dx = x + direction[0];
-                int dy = y + direction[1];
-                if(dx >= 0 && dx < grid.length && dy >= 0 && dy < grid.length && grid[dx][dy] >= 0){
-                    que.offer(new int[]{dx,dy,length+1});
+        // 2. bfs to expand this island
+        int step = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                int[] cur = q.poll();
+                for (int[] dir : dirs) {
+                    int i = cur[0] + dir[0];
+                    int j = cur[1] + dir[1];
+                    if (i >= 0 && j >= 0 && i < m && j < n && !visited[i][j]) {
+                        if (A[i][j] == 1) {
+                            return step;
+                        }
+                        q.offer(new int[]{i, j});
+                        visited[i][j] = true;
+                    }
                 }
             }
+            step++;
         }
-        return shortestBridgeLength;
+        return -1;
     }
-    
-    public void dfs(int i, int j, int[][] grid){
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid.length || grid[i][j] == 0){
+    private void dfs(int[][] A, boolean[][] visited, Queue<int[]> q, int i, int j, int[][] dirs) {
+        if (i < 0 || j < 0 || i >= A.length || j >= A[0].length || visited[i][j] || A[i][j] == 0) {
             return;
         }
-        grid[i][j] = 0;
-        que.offer(new int[]{i,j,-1});
-        for(int[] dir : directions){
-            dfs(i+dir[0],j+dir[1],grid);
+        visited[i][j] = true;
+        q.offer(new int[]{i, j});
+        for (int[] dir : dirs) {
+            dfs(A, visited, q, i + dir[0], j + dir[1], dirs);
         }
     }
 }
