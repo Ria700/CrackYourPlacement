dff5f1fe45dd792ba89353af82277ef45886ba6d
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-07-02 11:43:26 +0530

    Time: 20 ms (55.18%), Space: 60.5 MB (62.05%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
-        Arrays.sort(horizontalCuts);
-        Arrays.sort(verticalCuts);
-        long ha = horizontalCuts[0], va = verticalCuts[0];
+        long ha = getMax(horizontalCuts, h),
+        va = getMax(verticalCuts, w);
+        
+        long mod = 1000000007;
+        return (int)(ha%mod * va%mod);
+    }
+    
+    public long getMax(int[] a, int l){
+        Arrays.sort(a);
+        long ans = a[0];
         int i = 1;
-        for(i = 1; i < horizontalCuts.length; i++){
-            ha = Math.max(ha, horizontalCuts[i] - horizontalCuts[i-1]);
+        for(i = 1; i < a.length; i++){
+            ans = Math.max(ans, a[i] - a[i-1]);
         }
-        ha = Math.max(ha, h - horizontalCuts[i-1]);
-        
-        for(i = 1; i < verticalCuts.length; i++){
-            va = Math.max(va, verticalCuts[i] - verticalCuts[i-1]);
-        }
-        va = Math.max(va, w - verticalCuts[i-1]);
-        
-        long ans = (ha*va)%1000000007;
-        return (int)ans;
+        ans = Math.max(ans, l - a[i-1]);
+        return ans;
     }
 }
