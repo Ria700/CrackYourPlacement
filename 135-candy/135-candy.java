95576ac68b8341ed0044708439eaaa554d4b0160
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-07-04 11:17:29 +0530

    Time: 5 ms (45.91%), Space: 51.4 MB (66.42%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -4,21 +4,20 @@ class Solution {
         int ans = 0;
         
         int[] left = new int[n];
-        int[] right = new int[n];
         
         Arrays.fill(left, 1);
-        Arrays.fill(right, 1);
         
         for(int i = 1; i < n; i++){
             if(ratings[i-1] < ratings[i]) left[i] = left[i-1] + 1;
         }
         
         for(int i = n-2; i >= 0; i--){
-            if(ratings[i+1] < ratings[i]) right[i] = right[i+1] + 1;
+            if(ratings[i+1] < ratings[i]) 
+                left[i] = Math.max(left[i+1] + 1, left[i]);
         }
         
         for(int i = 0; i < n; i++){
-            ans += Math.max(left[i], right[i]);
+            ans += left[i];
         }
         
         return ans;
