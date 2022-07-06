4cd1bd4950a6b14603d7f2f2df5c9503f135ff5c
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-07-06 12:17:19 +0530

    Time: 21 ms (41.09%), Space: 50.3 MB (60.24%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,18 +1,5 @@
 class Solution {
-    class Pair implements Comparable<Pair>{
-        int i, freq;
         
-        Pair(int a, int b){
-            i = a;
-            freq = b;
-        }
-        
-        public int compareTo(Pair p){
-            if(p.freq == this.freq) return this.i-p.i;
-            return this.freq - p.freq;
-        }
-    }
-    
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         
@@ -21,23 +8,24 @@ class Solution {
             map.put(i, freq+1);
         }
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(a) - map.get(b));
         for(int i: map.keySet()){
-            // pq.add(new Pair(i, map.get(i)));
-            // if(pq.size() > k) pq.poll();
-            int freq = map.get(i);
             if(pq.size() < k){
-                pq.offer(new Pair(i, freq));
-            }else if(pq.peek().freq < freq){
-                pq.offer(new Pair(i, freq));
-                pq.poll();
+                pq.add(i);
+            }
+            else{
+                int freq = map.get(pq.peek());
+                if(map.get(i) > freq){
+                    pq.poll();
+                    pq.add(i);
+                } 
             }
         }
         
         int j = 0;
         int[] ans = new int[k];
         while(pq.size() > 0){
-            ans[j++] = pq.poll().i;
+            ans[j++] = pq.poll();
         }
         return ans;
     }
