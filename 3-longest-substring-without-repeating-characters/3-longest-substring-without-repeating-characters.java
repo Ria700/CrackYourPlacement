11fa3ab9f99b2c5d8e739f862b755b7f755008a4
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-07-10 15:14:45 +0530

    Time: 17 ms (32.88%), Space: 46.2 MB (29.40%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,39 +1,20 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int max = 0;
-        int si = 0, ei = 0;
         HashSet<Character> set = new HashSet<>();
-        while(ei < s.length()){
-            char c = s.charAt(ei);
-            if(set.contains(c)){
-                int curr = ei-si;
-                if(curr > max) max = curr;
-                // set = new HashSet<>();  // we cant create a new hashset all together
-                // set.add(c);
-                // si = ei;
-                // ei++;
-                
-                // start removing from start (SLIDING WINDOW !!! - not just window)
-                // find = c
-                for(int i = si; i < ei; i++){
-                    char ch = s.charAt(i);
-                    if(ch == c){
-                        si++;
-                        break;
-                    }
-                    // nhi pooche ka kaam
-                    set.remove(ch);
-                    si++;
-                }
-            }else{
+        int max = 0, i = 0, j = 0;
+        while(j < s.length()){
+            char c = s.charAt(j);
+            if(!set.contains(c)) {
                 set.add(c);
+                j++;
+                max = Math.max(max, j-i);
+            }else{
+                while(set.contains(c)) {
+                    set.remove(s.charAt(i));
+                    i++;
+                }
             }
-            ei++;
         }
-        
-        int last = ei-si;
-        if(last > max) max = last;
-        
         return max;
     }
 }
