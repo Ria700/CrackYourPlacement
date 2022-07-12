2c556658d77ca99f325c6f2d78db2e12273cf3a8
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-07-13 00:25:24 +0530

    Time: 219 ms (38.08%), Space: 42.1 MB (38.60%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -1,39 +1,79 @@
 class Solution {
-    public boolean makesquare(int[] matchsticks) {
-        int total = 0;
+    public boolean makesquare(int[] nums) {
+    	if (nums == null || nums.length < 4) return false;
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if (sum % 4 != 0) return false;
         
-        for (int i : matchsticks) {
-            total += i;
-        }
+        Arrays.sort(nums);
+        reverse(nums);
         
-        if (total % 4 != 0) return false;
-        
-        Arrays.sort(matchsticks);
-        
-        return match(matchsticks, matchsticks.length-1, 0, 0, 0, 0, total/4);
+    	return dfs(nums, new int[4], 0, sum / 4);
     }
     
-    private boolean match(int[] matchsticks, int index, int top, int bottom, int left, int right, int target) {
-        
-        if (top == target && bottom == target && left == target && right == target) return true;
-        // 1) this way we dont have to check each sum
-        // 2) we dont even need to check i == n bec we're comapring total target
-        
-        if (top > target || bottom > target || left > target || right > target) return false;
-                        
-        int val = matchsticks[index]; // index
-        
-        boolean t = match(matchsticks, index - 1, top + val, bottom, left, right, target);
-        if (t) return true;
-        boolean b = match(matchsticks, index - 1, top, bottom + val, left, right, target);
-        if (b) return true;
-        boolean l = match(matchsticks, index - 1, top, bottom, left + val, right, target);
-        if (l) return true;
-        boolean r = match(matchsticks, index - 1, top, bottom, left, right + val, target);
-        if (r) return true;
-        
-        return false;
+    private boolean dfs(int[] nums, int[] sums, int index, int target) {
+    	if (index == nums.length) {
+    	    if (sums[0] == target && sums[1] == target && sums[2] == target) {
+    		return true;
+    	    }
+    	    return false;
+    	}
+    	
+    	for (int i = 0; i < 4; i++) {
+    	    if (sums[i] + nums[index] > target) continue;
+    	    sums[i] += nums[index];
+            if (dfs(nums, sums, index + 1, target)) return true;
+    	    sums[i] -= nums[index];
+    	}
+    	
+    	return false;
     }
+    
+    private void reverse(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++; j--;
+        }
+    }
+    
+//     public boolean makesquare(int[] matchsticks) {
+//         int total = 0;
+        
+//         for (int i : matchsticks) {
+//             total += i;
+//         }
+        
+//         if (total % 4 != 0) return false;
+        
+//         Arrays.sort(matchsticks);
+        
+//         return match(matchsticks, matchsticks.length-1, 0, 0, 0, 0, total/4);
+//     }
+    
+//     private boolean match(int[] matchsticks, int index, int top, int bottom, int left, int right, int target) {
+        
+//         if (top == target && bottom == target && left == target && right == target) return true;
+//         // 1) this way we dont have to check each sum
+//         // 2) we dont even need to check i == n bec we're comapring total target
+        
+//         if (top > target || bottom > target || left > target || right > target) return false;
+                        
+//         int val = matchsticks[index]; // index
+        
+//         boolean t = match(matchsticks, index - 1, top + val, bottom, left, right, target);
+//         if (t) return true;
+//         boolean b = match(matchsticks, index - 1, top, bottom + val, left, right, target);
+//         if (b) return true;
+//         boolean l = match(matchsticks, index - 1, top, bottom, left + val, right, target);
+//         if (l) return true;
+//         boolean r = match(matchsticks, index - 1, top, bottom, left, right + val, target);
+//         if (r) return true;
+        
+//         return false;
+//     }
     
 //     private boolean kSubsets(int arr[], int k, int ss, List<List<Integer>> ans, int i){
 //         if(i >= arr.length){
