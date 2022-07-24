4273a8bbec16a5577781549997201eed17e19861
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-07-24 16:12:28 +0530

    Time: 0 ms (100.00%), Space: 45.7 MB (32.68%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -15,28 +15,60 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Node dh = new Node(-1);
-        Node dt = dh, t1 = head, t2 = dh;
-        HashMap<Node, Node> map = new HashMap<>();
+        // Apporoach 1
+        // tc: O(n) sc: O(n): WITH HASHMAP
         
+        // Node dh = new Node(-1);
+        // Node dt = dh, t1 = head, t2 = dh;
+        // HashMap<Node, Node> map = new HashMap<>();
+        // while(t1!=null) {
+        //     Node nn = new Node(t1.val);
+        //     map.put(t1, nn);
+        //     dt.next = nn;
+        //     t1 = t1.next;
+        //     dt = dt.next;
+        // }
+        // t1 = head;
+        // t2 = dh.next;
+        // while(t1!=null) {
+        //     if(t1.random == null) t2.random = null;
+        //     else t2.random = map.get(t1.random);
+        //     t1 = t1.next;
+        //     t2 = t2.next;
+        // }
+        // return dh.next;
+        
+        // Apporoach 2
+        // tc: O(n) SC: O(1): WITHOUT HASHMAP
+        
+        // 1 - insert new nodes b/w og list
+        Node t1 = head;
         while(t1!=null) {
             Node nn = new Node(t1.val);
-            map.put(t1, nn);
-            dt.next = nn;
-            t1 = t1.next;
-            dt = dt.next;
+            nn.next = t1.next;
+            t1.next = nn;
+            t1 = t1.next.next;
         }
         
+        // 2 - place random pointers
         t1 = head;
-        t2 = dh.next;
-        
         while(t1!=null) {
-            if(t1.random == null) t2.random = null;
-            else t2.random = map.get(t1.random);
-            t1 = t1.next;
-            t2 = t2.next;
+            t1.next.random = t1.random == null? null : t1.random.next;
+            // t1's new node rests art its next!
+            t1 = t1.next.next;
         }
         
+        // 3 - segregate lists
+        Node dh = new Node(-1);
+        Node dt = dh;
+        t1 = head;
+        while(t1!=null) {
+            dt.next = t1.next;
+            t1.next = t1.next.next;
+            dt = dt.next;
+            t1 = t1.next;
+        }
+
         return dh.next;
     }
 }
