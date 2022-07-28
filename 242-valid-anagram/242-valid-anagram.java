ab5d8388e56cdca8314d9b23c4ff020fbdb2cdbb
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-07-28 10:46:31 +0530

    Time: 2 ms (99.09%), Space: 42.7 MB (89.02%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -9,14 +9,24 @@ class Solution {
         // M3-Sorting
         
         // M-2
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(char c: s.toCharArray()) map.put(c, map.getOrDefault(c,0)+1);
-        for(char c: t.toCharArray()) {
-            int freq = map.getOrDefault(c,0);
-            if(freq == 0) return false;
-            map.put(c, freq-1);
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // for(char c: s.toCharArray()) map.put(c, map.getOrDefault(c,0)+1);
+        // for(char c: t.toCharArray()) {
+        //     int freq = map.getOrDefault(c,0);
+        //     if(freq == 0) return false;
+        //     map.put(c, freq-1);
+        // }
+        // return true;
+        
+        // M-3
+        char[] sarr = s.toCharArray();
+        char[] tarr = t.toCharArray();
+        // char arry string - new String(sarr);
+        Arrays.sort(sarr);
+        Arrays.sort(tarr);
+        for(int i = 0; i < sarr.length; i++) {
+            if(sarr[i]!=tarr[i]) return false;
         }
         return true;
-        
     }
 }
