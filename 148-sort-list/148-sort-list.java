2fd29b69ebcdfa093ee874cd4ef16f8e5b795c4a
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-07-28 18:14:32 +0530

    Time: 1312 ms (5.10%), Space: 76.8 MB (51.97%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -10,40 +10,79 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null) return head;
-        ListNode mid = midPoint(head);
-        ListNode sh = mid.next;
-        mid.next = null;
-        ListNode lh = sortList(head), rh = sortList(sh);
-        return mergeList(lh, rh);
+        return quickSort(head)[0];
     }
     
-    private ListNode mergeList(ListNode l1, ListNode l2) {
-        ListNode dh = new ListNode(-1);
-        ListNode dt = dh;
+    private ListNode[] quickSort(ListNode head) { // returns arr[2]= {head, tail}
+        // Base Case
+        if(head == null || head.next == null) return new ListNode[]{head, head};
+        int l = len(head);
+        ListNode[] metaData = segregate(head, l/2);
+        ListNode[] small = quickSort(metaData[0]);
+        ListNode[] large = quickSort(metaData[2]);
+        return mergeSortedLists(small, metaData[1], large);
+    }
+    
+    private int len(ListNode head) {
+        if(head == null) return 0;
+        ListNode curr = head;
+        int len = 0;
+        while(curr != null) {
+            len++;
+            curr = curr.next;
+        }
+        return len;
+    }
+    
+    private ListNode[] segregate(ListNode head, int pivotPos) { // returns arr[3]= {smallHead, pivot, largeHead}
         
-        ListNode t1 = l1, t2 = l2;
-        while(t1!=null && t2!=null) {
-            if(t1.val < t2.val) {
-                dt.next = t1;
-                t1 = t1.next;
-            } else {
-                dt.next = t2;
-                t2 = t2.next;
-            }
-            dt=dt.next;
+        ListNode pivot = head;
+        while(pivotPos-->0) {
+            pivot = pivot.next;
         }
-        if(t1!=null) dt.next = t1;
-        else if(t2!=null) dt.next = t2;
-        return dh.next;
+        ListNode curr = head;
+        ListNode smaller = new ListNode(-1);
+        ListNode larger = new ListNode(-1);
+        ListNode st = smaller, lt = larger;
+        while(curr != null) {
+            if(curr != pivot){
+                if(curr.val > pivot.val) {
+                    lt.next = curr;
+                    lt = lt.next;
+                } else {
+                    st.next = curr;
+                    st = st.next;
+                }
+            }
+            curr = curr.next;
+        }
+        
+        st.next = null;
+        lt.next = null;
+        pivot.next = null;
+        
+        return new ListNode[]{smaller.next, pivot, larger.next};
     }
     
-    private ListNode midPoint(ListNode head) {
-        ListNode small = head, fast = head;
-        while(fast.next != null && fast.next.next != null) {
-            small = small.next;
-            fast = fast.next.next;
+    private ListNode[] mergeSortedLists(ListNode[] small, ListNode pivotNode, ListNode[] large) { // returns arr[2]= {head, tail}
+        ListNode head = null, tail = null;
+        if(small[0] != null && large[0] != null) {
+            small[1].next = pivotNode;
+            pivotNode.next = large[0];
+            head = small[0];
+            tail = large[1];
+        } else if(small[0] != null) {
+            small[1].next = pivotNode;
+            head = small[0];
+            tail = pivotNode;
+        } else if(large[0] != null) {
+            pivotNode.next = large[0];
+            head = pivotNode;
+            tail = large[1];
+        } else {
+            head = tail = pivotNode;
         }
-        return small;
+        
+        return new ListNode[]{head, tail};  
     }
 }
