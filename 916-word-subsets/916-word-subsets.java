aac173e176b73e7be8ffa91120056a491ffa106b
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-07-30 10:15:19 +0530

    Time: 33 ms (66.26%), Space: 85.4 MB (74.80%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        List<String> ans = new ArrayList<String>();
+        char[] w2 = new char[26];
+        for(String word: words2) {
+            char[] temp = new char[26];
+            for(char c: word.toCharArray()) {
+                temp[c-'a']++;
+            }
+            for(int k = 0; k < 26; k++) {
+                if(temp[k]>w2[k]) w2[k] = temp[k];
+            }
+        }
+        
+        for(String word: words1) {
+            char[] w1 = new char[26];
+            for(char c: word.toCharArray()) {
+                w1[c-'a']++;
+            }
+            
+            int i = 0, j = 0;
+            for(; i < 26; i++) {
+                if(w2[i]!=0) {
+                    if(w2[i] > w1[i]) {
+                        j = 1;
+                        break;
+                    }
+                }
+            }
+            
+            if(j == 0) ans.add(word);
+        }
+        
+        return ans;
+    }
+}
