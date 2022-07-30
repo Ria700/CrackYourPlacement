2a30d1217a3e8140550de5bcca494ada49c075e2
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-07-30 21:47:22 +0530

    Time: 1 ms (99.71%), Space: 42.2 MB (96.07%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,29 +1,11 @@
 class Solution {
-    static class pair implements Comparable<pair>{
-        int val, idx;
-        
-        pair(int a, int b) {
-            val = a;
-            idx = b;
-        }
-        
-        public int compareTo(pair o) {
-            if(this.val == o.val) return this.idx-o.idx;
-            return this.val-o.val;
-        }
-    }
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        pair arr[] = new pair[n];
-        for(int i = 0; i < n; i++) arr[i] = new pair(nums[i], i);
-        Arrays.sort(arr);
-        int i = 0, j = n-1;
-        while(i < j) {
-            int sum = arr[i].val + arr[j].val;
-            if(sum == target) return new int[]{arr[i].idx, arr[j].idx};
-            else if(sum > target) j--;
-            else i++;
+        HashMap<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int b = nums[i], a = target - b;
+            if (seen.containsKey(a)) return new int[]{seen.get(a), i}; // Found pair of (a, b), so that a + b = target
+            seen.put(b, i);
         }
-        return null;
+        return new int[]{};
     }
 }
