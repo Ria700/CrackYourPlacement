8f18ec9f3e2a7a89a551284d9ea3aa154c98d9b0
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-07-31 15:23:15 +0530

    Time: 191 ms (21.37%), Space: 137.4 MB (9.40%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -1,15 +1,20 @@
+// SEGMENT TREE - cant believe i could solve ths question
+// (Also July LeetCode Challenge Finale question)
 class NumArray {
-//     static class Node {
-//         int val;
-//         Node left, right;
-        
-//         Node(int val) {
-//             this.val = val;
-//         }
-//     }
+    // no node needed - can just form tree using array(like heap)
+    // static class Node {
+    //     int val;
+    //     Node left, right;
+    //     Node(int val) {
+    //         this.val = val;
+    //     }
+    // }
     
+    // tree in form of array just like heap
     int tree[];
+    // so that we can access `nums`
     int a[];
+    // length of array - also max range
     int n;
     public NumArray(int[] nums) {
         n = nums.length;
@@ -17,26 +22,32 @@ class NumArray {
         tree = new int[n*4];
         build(1, 0, n-1);
     }
-    
+     // build segment tree
     public void build(int node, int low, int high) {
         if(low == high) {
+            // base node
             tree[node] = a[low];
         } else if(low < high) {
             int left = node*2;
             int right = node*2+1;
             int mid = low + (high-low)/2;
+            // bulid left
             build(left, low, mid);
+            // bulid right
             build(right, mid+1, high);
+            // build curr by just accessing the left and right nodes
             tree[node] = tree[left] + tree[right];
         }
     }
     
+    // 1
     public void update(int index, int val) {
         _update(1, 0, n-1, index, val);
     }
     
     private void _update(int node, int st, int en, int index, int val) {
         if(st == en) {
+            //update
             a[st] = val;
             tree[node] = val;
         } else {
@@ -46,18 +57,23 @@ class NumArray {
             }else {
                 _update(node*2+1, mid+1, en, index, val);
             }
+            //backtrack update
             tree[node] = tree[node*2]+tree[node*2+1];
         }
     }
     
+    // 2
     public int sumRange(int left, int right) {
         return _sumRange(1, 0, n-1, left, right);
     }
     
+    // left and right are query - remains same through-out
+    // st & en change for defining the curr (valid) range
     private int _sumRange(int node, int st, int en, int l, int r) {
-        if(en < l|| st > r) return 0;
-        else if(st == en) return tree[node];
-        else if(l<=st && en<=r) return tree[node];
+        if(en < l|| st > r) return 0; // inavlid range
+        else if(st == en) return tree[node]; // single node
+        else if(l<=st && en<=r) return tree[node]; // lies within range
+        // else - calculate for range
         int left = node*2;
         int right = node*2+1;
         int mid = st + (en-st)/2;
