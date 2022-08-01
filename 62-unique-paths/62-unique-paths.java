5f09ca9c4d78236f2e14d6c00b364f0c4ea1a45e
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-01 11:12:43 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (27.16%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,12 +1,14 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m][n];
-        Arrays.fill(dp[m-1], 1);
-        for(int i = m-2; i >= 0; i--) dp[i][n-1] = 1;
-        for(int i = m-2; i >= 0; i--) 
-            for(int j = n-2; j >= 0; j--)
-                dp[i][j] = dp[i+1][j] + dp[i][j+1];
-        return dp[0][0];
-        
+        int N = n + m - 2;// how much steps we need to do
+        int k = m - 1; // number of steps that need to go down
+        double res = 1;
+        // here we calculate the total possible path number 
+        // Combination(N, k) = n! / (k!(n - k)!)
+        // reduce the numerator and denominator and get
+        // C = ( (n - k + 1) * (n - k + 2) * ... * n ) / k!
+        for (int i = 1; i <= k; i++)
+            res = res * (N - k + i) / i;
+        return (int)res;
     }
 }
