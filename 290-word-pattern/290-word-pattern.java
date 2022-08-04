5ee8390a08324cebf156700050146040209de786
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-04 15:31:45 +0530

    Time: 2 ms (35.27%), Space: 42.6 MB (6.33%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -1,32 +1,23 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-//         HashMap<Character,String> map1 = new HashMap<>();
-//         HashMap<String, Character> map2 = new HashMap<>();
-//         String[] a = s.split(" ", 0);
-//         int i = 0;
-//         for(String word: a) {
-//             if(i >= pattern.length()) return false;
-//             char ch = pattern.charAt(i);
-//             if(map1.containsKey(ch) || map2.containsKey(word)) {
-//                 if(!(map1.getOrDefault(ch, " ")).equals(word) || map2.getOrDefault(word, ' ')!=ch) return false;
-//             } else {
-//                 map1.put(ch, word);
-//                 map2.put(word, ch);
-//             }
-            
-//             i++;
-//         }
-//         if(i < pattern.length()) return false;
-//         return true;
-        
-        // Single map can work as well
-        String[] words = s.split(" ");
-        if (words.length != pattern.length())
-            return false;
-        Map index = new HashMap();
-        for (Integer i=0; i<words.length; ++i)
-            if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
-                return false;
+        HashMap<Character,String> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
+        String[] a = s.split(" ", 0);
+        if(pattern.length() != a.length) return false;
+        int i = 0;
+        for(String word: a) {
+            char ch = pattern.charAt(i);
+            if(map.containsKey(ch)) {
+                if(!map.get(ch).equals(word)) return false;
+            } else {
+                if(set.contains(word)) return false;
+                else {
+                    map.put(ch, word);
+                    set.add(word);
+                }
+            }
+            i++;
+        }
         return true;
     }
 }
