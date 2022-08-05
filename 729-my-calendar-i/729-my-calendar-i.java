a04077247be97923a91c39a34611f98c6e5617ab
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-05 20:34:40 +0530

    Time: 23 ms (94.22%), Space: 42.9 MB (94.90%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,16 +1,48 @@
 class MyCalendar {
-    TreeMap<Integer, Integer> calendar;
-
-    MyCalendar() {
-        calendar = new TreeMap();
+    ArrayList<int[]> calendar;
+    public MyCalendar() {
+        calendar = new ArrayList<>();
     }
-
+    
     public boolean book(int start, int end) {
-        Integer prev = calendar.floorKey(start),
-                next = calendar.ceilingKey(start);
-        if ((prev == null || calendar.get(prev) <= start) &&
-                (next == null || end <= next)) {
-            calendar.put(start, end);
+        if(calendar.size() == 0) {
+            calendar.add(new int[]{start, end});
+            return true;
+        }
+        
+        if(calendar.size() == 1) {
+            int prev[] = calendar.get(0);
+            if((prev[1] > start && prev[0] < start) || (end > prev[0] && end <= prev[1])) {
+                return false;
+            }   
+            if(prev[0] >= end) calendar.add(0, new int[]{start, end});
+            else calendar.add(new int[]{start, end});
+            return true;
+        }
+        
+        int i = 0;
+        while(i < calendar.size() && calendar.get(i)[0]<start){
+            i++;
+        }
+        if(i == 0) {
+            if(end <= calendar.get(0)[0]) {
+                calendar.add(0, new int[]{start, end});
+                return true;
+            }
+            else return false;
+        }
+        
+        if(i == calendar.size()) {
+            if(start >= calendar.get(calendar.size()-1)[1]) {
+                calendar.add(new int[]{start, end});
+                return true;
+            }
+            else return false;
+        }
+        int prev[] = calendar.get(i-1);
+        int next[] = calendar.get(i);
+        if(prev[1] <= start && end <= next[0]) {
+            calendar.add(i, new int[]{start, end});
             return true;
         }
         return false;
