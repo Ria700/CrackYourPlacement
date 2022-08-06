15d8314802e5b4574534a1663ea9bfd44b70d62c
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-06 12:43:11 +0530

    Time: 20 ms (43.33%), Space: 54.5 MB (74.74%) - LeetHub

--- 554-brick-wall/554-brick-wall.java
+++ 554-brick-wall/554-brick-wall.java
@@ -1,21 +1,14 @@
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        int width = 0;
-        for(int i: wall.get(0)) {
-            width+=i;
-        }
         for(List<Integer> list: wall) {
             int ps = 0;
-            for(int i: list) {
-                ps+=i;
-                if(ps < width) map.put(ps, map.getOrDefault(ps,0)+1);
+            for(int i = 0;  i < list.size()-1; i++) { // travel only till the last element to avoid last line of cross = no need for width
+                ps+=list.get(i);
+                map.put(ps, map.getOrDefault(ps,0)+1);
             }
         }
         int maxGaps = 0;
-        // for(int i = 1; i < width; i++) {
-        //     if(map.getOrDefault(i,0) > max) max = map.get(i);
-        // }
         for(int i : map.keySet()) {
             if(map.get(i) > maxGaps) maxGaps = map.get(i);
         }
