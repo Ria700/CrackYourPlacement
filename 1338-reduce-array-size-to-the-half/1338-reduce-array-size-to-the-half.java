1313075de3ec51bdcab178b4fe08109546b6f962
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-18 14:00:24 +0530

    Time: 59 ms (76.34%), Space: 84.4 MB (65.96%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,21 +1,17 @@
 class Solution {
     public int minSetSize(int[] arr) {
-        int[] count = new int[100001];
-        for(int a : arr) {
-            count[a]++;
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int i: arr) {
+            map.put(i, map.getOrDefault(i,0)+1);
         }
-        
-        Arrays.sort(count);
-        
         int n = arr.length;
-        int sum = 0;
-        int i = 0;
-        for(i = count.length - 1; i>=0; i--) {
-            sum += count[i];
-            if(sum >= n/2)
-                break;
+        ArrayList<Integer> a = new ArrayList(map.values());
+        Collections.sort(a);
+        int i = 0, sum = 0; // sum of frequencies
+        for(i = a.size()-1; i >= 0; i--) {
+            sum+=a.get(i);
+            if(sum >= n/2) break;
         }
-        
-        return count.length - i;
+        return a.size()-i;
     }
 }
