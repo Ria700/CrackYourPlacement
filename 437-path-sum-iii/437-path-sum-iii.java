f1437a9ee34b959e464ccd88c21107ee3a16d53d
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-18 12:35:29 +0530

    Time: 27 ms (32.51%), Space: 44 MB (62.94%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    public int pathSum(TreeNode root, int targetSum) {
+
+        countTarget_temp(root , targetSum);
+        return count;
+    }
+    public void countTarget_temp(TreeNode node , int targetSum){
+        if(node == null){
+            return;
+        }
+        //if(node.val == -2)
+        countTarget(node , 0 , targetSum);
+
+        countTarget_temp(node.left , targetSum);
+        countTarget_temp(node.right , targetSum);
+    }
+
+    public void countTarget(TreeNode node , long csum , int targetSum){
+        if(node == null){
+
+            return;
+        }
+        //System.out.println(node.val + " " + csum);
+        if(csum+node.val == targetSum){
+            //System.out.println(csum  + " " + node.val);
+            count++;
+        }
+        countTarget(node.left , csum+node.val , targetSum);
+        countTarget(node.right , csum+node.val , targetSum);
+    }
+}
