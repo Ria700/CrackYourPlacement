e6d49ecaaf63044743929738413a1e377bbb5465
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-20 17:03:24 +0530

    Time: 294 ms (8.68%), Space: 150.3 MB (49.48%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,23 +1,26 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        Deque<Integer> q = new LinkedList<>();
-        int ans[] = new int[n-k+1];
-        int p = 0;
-        for(int i = 0; i <= n; i++){
-            if(i < k){
-                while(q.size() > 0 && nums[i] >= nums[q.peekLast()]) q.removeLast();
-                q.addLast(i);
-            }else{
-                ans[p++] = nums[q.peekFirst()];
-                if(i == n) break;
-                
-                while(q.size() > 0 && nums[i] >= nums[q.peekLast()]) q.removeLast();
-                while(q.size() > 0 && q.peekFirst() < i-k+1) q.removeFirst();
-                q.addLast(i);
+        int[] nge = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for(int i = n-1; i >= 0; i--) {
+            while(!stack.isEmpty() && nums[i] >= nums[stack.peek()]) {
+                stack.pop();
             }
+            
+            if(stack.isEmpty()) nge[i] = n;
+            else nge[i] = stack.peek();
+            
+            stack.push(i);
         }
         
+        int[] ans = new int[n-k+1];
+        int j = 0;
+        for(int i = 0; i <= n-k; i++) {
+            if(j<i) j = i;
+            while(nge[j] < i+k) j = nge[j];
+            ans[i] = nums[j];
+        }
         return ans;
     }
 }
