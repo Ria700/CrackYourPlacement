09f3f76cad815ccece8a30f6dcfa3b74daa5e688
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-22 22:25:05 +0530

    Time: 3 ms (11.07%), Space: 40.6 MB (83.21%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        return ((n>0) && ((n & n-1)==0) && (n%3 == 1));
+        while (n % 4 == 0 && n != 0) {
+            n /= 4;
+        }
+        return n == 1;
     }
 }
