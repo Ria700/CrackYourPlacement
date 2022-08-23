2e6b2b48199690fa9b041082157c62b13ded9aca
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-23 16:14:21 +0530

    Time: 5 ms (57.66%), Space: 44.7 MB (79.16%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.java
+++ 946-validate-stack-sequences/946-validate-stack-sequences.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         Stack<Integer> s = new Stack<>();
-        int i = 0, j = 0;
-        while(i < pushed.length) {
-            s.push(pushed[i++]);
-            while(!s.isEmpty() && s.peek() == popped[j]) {
+        int i = 0, j = 0, c = 0;
+        while(j < pushed.length) {
+            if(!s.isEmpty() && s.peek() == popped[j]) {
                 s.pop();
                 j++;
+                c++;
+            } else if(i < pushed.length) {
+                s.push(pushed[i++]);
+            } else {
+                return false;
             }
         }
-        while(!s.isEmpty()) {
-            if(s.pop() != popped[j++]) return false;
-        }
-        if(j == popped.length) return true;
-        return false;
+        return c == pushed.length;
     }
 }
