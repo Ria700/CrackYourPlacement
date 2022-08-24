9c3f989b07209d996b249079cbb7f1fc6ba0fe00
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-24 12:51:37 +0530

    Time: 30 ms (31.79%), Space: 42.8 MB (88.40%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch: s.toCharArray()) {
+            if(ch == 'c') {
+                if(st.size() > 0 && st.peek() == 'b') {
+                    st.pop();
+                    if(st.size() > 0 && st.peek() == 'a') st.pop();
+                    else {
+                        st.push('b');
+                        st.push('c');
+                    }
+                } else 
+                    st.push('c');
+            } else st.push(ch);
+        }
+        return st.size() == 0;
+    }
+}
