c2a86059e25f24640b53ecb9e85634bd253cd451
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-25 19:51:35 +0530

    Time: 20 ms (66.69%), Space: 51.8 MB (67.63%) - LeetHub

--- 636-exclusive-time-of-functions/636-exclusive-time-of-functions.java
+++ 636-exclusive-time-of-functions/636-exclusive-time-of-functions.java
@@ -1,28 +1,33 @@
+class meta {
+    int id, stTime, wt;
+    meta() {
+        
+    }
+    meta(int a, int b, int c) {
+        id = a;
+        stTime = b;
+        wt = c;
+    }
+}
+
 class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
-        Stack<Integer> st = new Stack<>(); // callStack
+        Stack<meta> st = new Stack<>(); // callStack
         int ans[] = new int[n];
-        int lastTime = -1;
+        
         for(String log: logs) {
             String data[] = log.split(":");
             int funcId = Integer.parseInt(data[0]);
             boolean isStart = "start".equals(data[1]);
             int timeStamp = Integer.parseInt(data[2]);
             
-            if(!isStart) timeStamp+=1;
-            
-            if(!st.isEmpty()) {
-                int currentFn = st.peek();
-                ans[currentFn] += timeStamp-lastTime;
-            }
-            
-            if(isStart) {
-                st.push(funcId);
-            } else {
-                st.pop();
-            }
-            
-            lastTime = timeStamp;
+            if(isStart) st.push(new meta(funcId, timeStamp, 0));
+            else {
+                meta fn = st.pop();
+                int ext = timeStamp-fn.stTime + 1 ;
+                ans[fn.id] += ext- fn.wt;
+                if(!st.isEmpty()) st.peek().wt += ext;
+            }           
         }
         return ans;
     }
