eacf14771da864fa3c4c219354a08564c89c9437
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-27 23:48:45 +0530

    Time: 100 ms (97.56%), Space: 46.5 MB (96.59%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -2,7 +2,7 @@ class Solution {
     public int countPrimes(int n) {
         if(n == 0 || n == 1) return 0;
         boolean notprime[] = new boolean[n];
-        int count = n-2;
+        int count = n-2; // one foe 1, two for strictly less than n
         for(int i = 2; i*i <= n; i++) {
             if(!notprime[i]) {
                 for(int j = 2*i; j < n; j+= i) {
