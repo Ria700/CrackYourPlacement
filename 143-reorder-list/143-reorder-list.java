0d6c35acd84f672bf08ae15f3ca739a3329c88b8
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-27 13:58:21 +0530

    Time: 2 ms (77.80%), Space: 54.3 MB (42.37%) - LeetHub

--- 143-reorder-list/143-reorder-list.java
+++ 143-reorder-list/143-reorder-list.java
@@ -9,60 +9,31 @@
  * }
  */
 class Solution {
+    ListNode left;
+    boolean stop;
     public void reorderList(ListNode head) {
-        // midpoint
-        ListNode mid = midPoint(head);
-        
-        // break
-        ListNode sh = mid.next;
-        mid.next = null;
-        
-        // reverse
-        sh = reverseList(sh);
-        
-        // merge
-        ListNode dh = new ListNode(-1);
-        ListNode dt = dh;
-        boolean flag = true;
-        while(head!=null || sh!=null) { //terminate only when both are null
-                                    //flag saves null pointer exception
-            if(flag) {
-                dt.next = head;
-                head = head.next;
-                flag = false;
-            } else {
-                dt.next = sh;
-                sh = sh.next;
-                flag = true;
-            }
-            dt=dt.next;
-        }
-        
-        // since we have managed pointers LL is reorderd
-        // head = dh.next;
-            
+        left = head;
+        stop = false;
+        helper(head);
     }
     
-    private ListNode midPoint(ListNode head) {
-        ListNode small = head, fast = head;
-        while(fast.next != null && fast.next.next != null) {
-            small = small.next;
-            fast = fast.next.next;
-        }
-        return small;
-    }
-    
-    private ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null) return head;
+    private void helper(ListNode right) {
+        if(right == null) return;
         
-        ListNode curr = head, prev = null;
-        while(curr!=null) {
-            ListNode temp = curr.next;
-            curr.next = prev;
-            
-            prev = curr;
-            curr = temp;
+        helper(right.next);
+        
+        if(stop) return;
+        
+        if(left == right || left.next == right) {
+            stop = true;
+            right.next = null;
+            return;
         }
-        return prev;
+        
+        ListNode next = left.next;
+        left.next = right;
+        right.next = next;
+        
+        left = next;
     }
 }
