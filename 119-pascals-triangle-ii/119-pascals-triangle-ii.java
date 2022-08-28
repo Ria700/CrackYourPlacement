1d9f9d49891959ee3100f0d92157b77e2794dac0
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-28 12:26:56 +0530

    Time: 2 ms (59.00%), Space: 41.7 MB (63.90%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -4,7 +4,7 @@ class Solution {
         
         for(int i = 0; i <= rowIndex; i++) {
             row.add(0,1);
-            for(int j = 1; j < row.size()-1; j++) {
+            for(int j = 1; j < i; j++) {
                 row.set(j, row.get(j)+row.get(j+1));
             }
         }
