8fe9c7a9675bea4edda1b6f181f35854e59851fb
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-28 16:23:25 +0530

    Time: 5 ms (63.38%), Space: 54.9 MB (37.12%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.java
+++ 986-interval-list-intersections/986-interval-list-intersections.java
@@ -5,12 +5,10 @@ class Solution {
         LinkedList<int[]> list = new LinkedList<>();
         
         while(i < a.length && j < b.length) {
-            if(b[j][1] <= a[i][0] || b[j][0] <= a[i][1]) {
-                int[] interval = new int[2];
-                interval[0] = Math.max(a[i][0], b[j][0]);
-                interval[1] = Math.min(a[i][1], b[j][1]);
-                if(interval[0] <= interval[1]) list.add(interval);
-            }
+            int lo = Math.max(a[i][0], b[j][0]);
+            int hi = Math.min(a[i][1], b[j][1]); 
+            if(lo <= hi) list.add(new int[]{lo, hi});
+            
             if(a[i][1] < b[j][1]) i++; // compare ends
             else j++;
         }
