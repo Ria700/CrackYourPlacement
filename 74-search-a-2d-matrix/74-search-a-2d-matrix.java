9fa28ac03e7576e2bc37e52f30d3e0604f7f8423
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-29 15:48:30 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (44.50%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,28 +1,55 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int st = 0, en = m-1, mid = 0;
-        while(st <= en) {
-            mid = st + (en-st)/2;
-            if(matrix[mid][0] == target) return true;
-            if(matrix[mid][0] < target) {
-                if(mid == m-1 || matrix[mid+1][0] > target)
-                    break;
-                else st = mid+1;
-            } else en = mid-1;
+        int pr = findPotentialRow(matrix,target); //log n
+        
+        if(pr == -1) {
+            return false;
         }
         
-        return binarySearch(matrix[mid], target);
+        return binarySearch(matrix,pr,target); //log m
     }
     
-    private boolean binarySearch(int[] arr, int target) {
-        int st = 0, en = arr.length-1;
-        while(st <= en) {
-            int mid = st + (en-st)/2;
-            if(arr[mid] == target) return true;
-            else if(arr[mid] > target) en = mid-1;
-            else st = mid+1;
+    public static int findPotentialRow(int[][]matrix,int target) {
+        int lo = 0;
+        int hi = matrix.length-1;
+        int lc = matrix[0].length-1;
+        
+        while(lo <= hi) {
+            
+            int m = (lo + hi)/2;
+            
+            if(matrix[m][0] <= target && matrix[m][lc] >= target) {
+                return m;
+            }
+            else if(matrix[m][0] > target) {
+                hi = m-1;
+            }
+            else {
+                lo = m+1;
+            }
         }
+        
+        return -1;
+    }
+    public static boolean binarySearch(int[][]matrix,int r,int target) {
+        int lo = 0;
+        int hi = matrix[0].length-1;
+        
+        while(lo <= hi) {
+            
+            int m = (lo + hi)/2;
+            
+            if(matrix[r][m] == target) {
+                return true;
+            }
+            else if(matrix[r][m] < target) {
+                lo = m+1;
+            }
+            else {
+                hi = m-1;
+            }
+        }
+        
         return false;
     }
 }
