368ddfc48d00cdb3b6ca4ab9c8206be048b778ba
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-29 13:19:40 +0530

    Time: 6 ms (85.44%), Space: 56.4 MB (14.86%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.java
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.java
@@ -4,17 +4,21 @@ class Solution {
         
         int ans[] = new int[nums.length];
         int i = nums.length-1;
+        // travel odd indices & fill sorted array's right half in it
         int idx = 1;
-        for(; idx < nums.length; i--) {
+        while(idx < nums.length) {
             ans[idx] = nums[i];
             idx+=2;
+            i--;
         }
+        // travel even indices & fill sorted array's left half in it
         idx = 0;
         while(idx < nums.length) {
             ans[idx] = nums[i];
             idx+=2;
             i--;
         }
+        // copy elements
         for(i = 0; i < nums.length; i++) {
             nums[i] = ans[i];
         }
