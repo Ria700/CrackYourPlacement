00ab40dea391a8d607b1a1b380e3040615cf4779
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-30 15:31:42 +0530

    Time: 4 ms (40.05%), Space: 42.3 MB (43.71%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,61 +1,30 @@
-import java.util.*;
 class Solution {
-    public String decodeString(String s)
-    {
-        Vector<Character> st = new Vector<Character>();
-         
-        for(int i = 0; i < s.length(); i++)
-        {
-             
-            // When ']' is encountered, we need to
-            // start decoding
-            if (s.charAt(i) == ']')
-            {
-                String temp = "";
-                while (st.size() > 0 && st.get(st.size() - 1) != '[')
-                {
-                     
-                    // st.top() + temp makes sure that the
-                    // string won't be in reverse order eg, if
-                    // the stack contains 12[abc temp = c + "" =>
-                    // temp = b + "c" => temp = a + "bc"
-                    temp = st.get(st.size() - 1) + temp;
-                    st.remove(st.size() - 1);
+    public String decodeString(String s) {
+        Stack<String> st = new Stack<>();
+        for(int i = s.length()-1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            if(ch == '[') {
+                String inside = "";
+                while(! "]".equals(st.peek())) {
+                    inside+=st.pop();
                 }
-                 
-                // Remove the '[' from the stack
-                st.remove(st.size() - 1);
-                String num = "";
-                 
-                // Remove the digits from the stack
-                while (st.size() > 0 &&
-                       st.get(st.size() - 1) >= 48 &&
-                       st.get(st.size() - 1) <= 57)
-                {
-                    num = st.get(st.size() - 1) + num;
-                    st.remove(st.size() - 1);
+                st.pop();
+                if(i > 0 && Character.isDigit(s.charAt(i-1))) {
+                    int p = 1, rep = 0;
+                    while(i > 0 && Character.isDigit(s.charAt(i-1))) {
+                        i--;
+                        rep = rep + (s.charAt(i)-'0')*p;
+                        p*=10;
+                    }
+                    String ninside = "";
+                    for(int j = 1; j <= rep; j++) ninside+=inside;
+                    inside = ninside;
                 }
-                 
-                int number = Integer.parseInt(num);
-                String repeat = "";
-                for(int j = 0; j < number; j++)
-                    repeat += temp;
-                     
-                for(int c = 0; c < repeat.length(); c++)
-                    st.add(repeat.charAt(c));
-            }
-             
-            // If s[i] is not ']', simply push
-            // s[i] to the stack
-            else
-                st.add(s.charAt(i));
+                st.push(inside);
+            } else st.push(ch+"");
         }
-        String res = "";
-        while (st.size() > 0)
-        {
-            res = st.get(st.size() - 1) + res;
-            st.remove(st.size() - 1);
-        }
-        return res;
+        String ans = "";
+        while(!st.isEmpty()) ans+=st.pop();
+        return ans;
     }
 }
