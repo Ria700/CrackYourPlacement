126a74173da987ace8ad25f3a355489ee6390a85
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-30 16:39:59 +0530

    Time: 0 ms (100.00%), Space: 60.6 MB (37.19%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,9 +1,25 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int ans = 0;
-        for(int i: nums) {
-            ans^=i; 
+        // binary search approach
+        int lo = 0, hi = nums.length-1;
+        while(lo <= hi) {
+            int mid = lo + (hi-lo)/2;
+            if(mid < nums.length-1 && nums[mid] == nums[mid+1]) {
+                int rh = hi-mid+1;
+                if(rh%2 == 0) {
+                    hi = mid-1;
+                } else {
+                    lo = mid+2;
+                }
+            } else if(mid > 0 && nums[mid] == nums[mid-1]) {
+                int rh = hi-mid;
+                if(rh%2 == 0) {
+                    hi = mid-2;
+                } else {
+                    lo = mid+1;
+                }
+            } else return nums[mid];
         }
-        return ans;
+        return -1;
     }
 }
