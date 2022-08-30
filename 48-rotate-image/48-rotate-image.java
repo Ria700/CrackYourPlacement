d69212f9cb8b2ed8386feab1061535df531fbc29
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-30 12:56:05 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (98.74%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,19 +1,28 @@
 class Solution {
     public void rotate(int[][] m) {
+        transpose(m);
+        reflect(m);
+    }
+    
+    private void reflect(int[][] m) {
         int n = m.length;
-        for(int i = 0; i < (n/2); i++) {
-            for(int j = i; j < (n-1-i); j++) {
-                int temp = m[i][j];
-                m[i][j] = m[n-1-j][i];
-                m[n-1-j][i] = m[n-1-i][n-1-j];
-                m[n-1-i][n-1-j] = m[j][n-1-i];
+        for(int i = 0; i < n/2; i++) { // column
+            for(int j = 0; j < n; j++) { // row
+                int temp = m[j][i];
+                m[j][i] = m[j][n-1-i];
                 m[j][n-1-i] = temp;
             }
         }
     }
     
-    // 1st -> m[i][j]
-    // 2nd -> m[j][n-1-i]
-    // 3rd -> m[n-1-i][n-1-j]
-    // 4th -> m[n-1-j][i]
+    private void transpose(int[][] m) {
+        int n = m.length;
+        for(int i = 0; i < n; i++) { // row
+            for(int j = i+1; j < n; j++) { // column
+                int temp = m[i][j];
+                m[i][j] = m[j][i];
+                m[j][i] = temp;
+            }
+        }
+    }
 }
