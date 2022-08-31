035f75302875f662abba0be93c90d145918affbc
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-31 22:59:42 +0530

    Time: 49 ms (26.76%), Space: 54.7 MB (8.46%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -6,22 +6,60 @@ class Solution {
         
         // Note: h cannot be less that piles.length
         
-        int lo = 1, hi = 0;
-        for(int i: piles) hi = Math.max(hi, i);
+//         int lo = 1, hi = 0;
+//         for(int i: piles) hi = Math.max(hi, i);
         
-        while(lo < hi) {
-            int mid = lo + (hi-lo)/2;
+//         while(lo < hi) {
+//             int mid = lo + (hi-lo)/2;
             
-            int noofhours = 0;
-            for(int i: piles) {
-                noofhours += Math.ceil((i*1.0)/mid);
-            }
+//             int noofhours = 0;
+//             for(int i: piles) {
+//                 noofhours += Math.ceil((i*1.0)/mid);
+//             }
              
-            if(noofhours > h) lo = mid+1; // zyada hrs lage 'h' se toh speed badhao range bdi pe rakho
-            else hi = mid; // kam ya equal hours lagre hain to we can decrease the speed further
+//             if(noofhours > h) lo = mid+1; // zyada hrs lage 'h' se toh speed badhao range bdi pe rakho
+//             else hi = mid; // kam ya equal hours lagre hain to we can decrease the speed further
+//         }
+        
+//         return lo;
+        
+         int max = piles[0];
+        
+        for(int val : piles) {
+            max = Math.max(max,val);
         }
         
-        return lo;
+        if(h == piles.length) {
+            return max;
+        }
+        
+        int lo = 0, hi = max;
+        int ans = 0;
+        
+        while(lo <= hi) {
+            int mid = lo + (hi - lo)/2;
+            
+            if(isPossible(piles,mid,h) == true) {
+                ans = mid;
+                hi = mid-1;
+            }
+            else {
+                lo = mid+1;
+            }
+        }
+        
+        return ans;
+    }
+    
+    
+    public static boolean isPossible(int[]piles,int sp,int h) {
+        int t = 0;
+        
+        for(int i=0; i < piles.length;i++) {
+            t += Math.ceil(piles[i]*1.0 / sp);
+        }
+        
+        return t <= h;
     }
     // initial thinking
     // we have to divide the array in h parts such that  the max is minimised
