db3ed9d680114d0c4350443f191e82d1589c54ab
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-08-31 22:09:40 +0530

    Time: 45 ms (42.50%), Space: 42.9 MB (98.15%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        // max value of k = max of array
+        // bec no matter what, i have to spend 1 hr on a pile 
+        // and this way i spend piles.length hours on the array
+        
+        // Note: h cannot be less that piles.length
+        
+        int lo = 0, hi = 0;
+        for(int i: piles) hi = Math.max(hi, i);
+        
+        while(lo < hi) {
+            int mid = lo + (hi-lo)/2;
+            
+            int noofhours = 0;
+            for(int i: piles) {
+                noofhours += Math.ceil((i*1.0)/mid);
+            }
+             
+            if(noofhours > h) lo = mid+1;
+            else hi = mid;
+        }
+        
+        return lo;
+    }
+    // initial thinking
+    // we have to divide the array in h parts such that  the max is minimised
+    // Note: we can divide upon a pile but not divide its bananas over other piles
+    // 3 3 4 3 3 4 3 3
+    // 4-6
+    // mid = 4
+        
+    // 4 11 20 23 30
+    // 4-25
+}
