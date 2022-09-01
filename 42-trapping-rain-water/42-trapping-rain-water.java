1d7bb0e6f397127ab3dab248577c52f64db9621d
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-09-01 12:41:59 +0530

    Time: 3 ms (25.99%), Space: 49.1 MB (34.95%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,27 +1,25 @@
 class Solution {
-    public int trap(int[] a) {
-        int n = a.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        int i = 0;
-        int peakSoFar = Integer.MIN_VALUE;
-        for(i = 0; i < n; i++){
-            if(a[i]>peakSoFar){
-                peakSoFar = a[i];
-            }
-            left[i]= peakSoFar;
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] maxLeft = new int[n];
+        int[] maxRight = new int[n];
+        maxLeft[0] = height[0];
+        maxRight[n-1] = height[n-1];
+        
+        for(int i = 1; i < n; i++) {
+            maxLeft[i] = Math.max(maxLeft[i-1], height[i]);
         }
-        peakSoFar = Integer.MIN_VALUE;
-        for(i = n-1; i >= 0; i--){
-            if(a[i]>peakSoFar){
-                peakSoFar = a[i];
-            }
-            right[i]= peakSoFar;
+        
+        for(int i = n-2; i >= 0; i--) {
+            maxRight[i] = Math.max(maxRight[i+1], height[i]);
         }
-        int sum = 0;
-        for(i = 0; i < n; i++){
-            sum += Math.min(left[i], right[i]) - a[i];
+        
+        int rainWater = 0;
+        for(int i = 1; i < n-1; i++) {
+            if(Math.min(maxLeft[i], maxRight[i]) > height[i])
+                rainWater += Math.min(maxLeft[i], maxRight[i])-height[i];
         }
-        return sum;
+        
+        return rainWater;
     }
 }
