df85a8d113c0640a54cfb367f1892504485b5098
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-09-02 16:38:42 +0530

    Time: 20 ms (29.28%), Space: 43.2 MB (44.36%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -5,17 +5,13 @@ class Solution {
         HashSet<Character> set = new HashSet<>();
         if(s.length() != t.length()) return false;
         int i = 0;
-        for(char ch1: s.toCharArray()) {
-            char ch2 = t.charAt(i);
-            if(map.containsKey(ch1)) {
-                if(map.get(ch1) != ch2) return false;
-            } else {
-                if(set.contains(ch2)) return false;
-                else {
-                    map.put(ch1, ch2);
-                    set.add(ch2);
-                }
-            }
+        for(char c: s.toCharArray()) {
+            char ref = t.charAt(i);
+            if(map.containsKey(c)) {
+               if(map.get(c) != ref) return false;
+            } else if(set.contains(ref)) return false;
+            map.put(c, ref);
+            set.add(ref);
             i++;
         }
         return true;
