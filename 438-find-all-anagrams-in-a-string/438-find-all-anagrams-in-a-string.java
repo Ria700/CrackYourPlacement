42fcbf7d2819a00b5ea12ea63067df32b636176f
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-09-04 11:02:10 +0530

    Time: 30 ms (44.24%), Space: 48.6 MB (35.28%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,56 +1,37 @@
 class Solution {
-    // Time Complexity: O(n)
-    // Space Complexity: O(k) k = pat.length
-    public List<Integer> findAnagrams(String txt, String pat) {
+    public List<Integer> findAnagrams(String s, String p) {
         List<Integer> ans = new ArrayList<>();
-        if(pat.length() > txt.length()) return ans;
-        
-        // Pattern Freq Map formation
         HashMap<Character, Integer> map = new HashMap<>();
-        HashMap<Character, Integer> window = new HashMap<>();
-        for(char c : pat.toCharArray()){
-            map.put(c, map.getOrDefault(c, 0)+1);
-        }
+        for(char c: p.toCharArray()) map.put(c, map.getOrDefault(c, 0)+1);
+        int count = map.size();
         
-        int k = pat.length(), size = txt.length(), i = 0, j = 0;
-        
-        // first window
-        while(j < k) {
-            char ch = txt.charAt(j);
-            window.put(ch, window.getOrDefault(ch, 0)+1);
+        int i = 0, j = 0;
+        while(j < s.length()) {
+            // acquire
+            char c = s.charAt(j);
+            
+            if(map.containsKey(c)) {
+                int freq = map.get(c);
+                map.put(c, freq-1);
+                if(freq == 1) count--;
+            }
+            
+            if(j-i == p.length()-1) {
+                // answer formation
+                if(count == 0) {
+                    ans.add(i);
+                }
+                
+                // release
+                char ch = s.charAt(i);
+                if(map.containsKey(ch)) {
+                    map.put(ch, map.get(ch)+1);
+                    if(map.get(ch) == 1) count++;
+                }
+                i++;
+            }
             j++;
         }
-        
-        while(j < size){
-            if(areMapsEqual(map, window)) ans.add(i);
-            
-            // remove
-            char ch = txt.charAt(i);
-            if(window.get(ch) == 1) window.remove(ch);
-            else window.put(ch, window.get(ch)-1);
-            
-            // add
-            ch = txt.charAt(j);
-            window.put(ch, window.getOrDefault(ch, 0)+1);
-            
-            i++;
-            j++;
-        }
-        
-        // last window
-        if(areMapsEqual(map, window)) ans.add(i);
         return ans;
-    }
-    
-    private boolean areMapsEqual(HashMap<Character, Integer> m1, HashMap<Character, Integer> m2) {
-        if(m1.size() != m2.size()) return false;
-        
-        for(Character ch: m1.keySet()) {
-            int v1 = m1.get(ch);
-            int v2 = m2.getOrDefault(ch, 0);
-            if(v1 != v2) return false;
-        }
-        
-        return true;
     }
 }
