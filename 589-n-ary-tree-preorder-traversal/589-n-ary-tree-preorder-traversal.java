ce4047274688b47070f2b0bc3ad3536738b579d1
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-09-05 11:48:42 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (90.14%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    List<Integer> ans;
+    public List<Integer> preorder(Node root) {
+        ans = new ArrayList<>();
+        if(root == null) return ans;
+        helper(root);
+        return ans;
+    }
+    
+    private void helper(Node root) {
+        ans.add(root.val);
+        for(Node child: root.children) {
+            helper(child);
+        }
+    }
+}
