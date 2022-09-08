46a1ec21d1cb2d7377a76dcf1cc697a34d7ce4b9
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-09-08 19:49:59 +0530

    Time: 3 ms (26.68%), Space: 49.6 MB (12.59%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,17 +1,30 @@
 class Solution {
     public int trap(int[] height) {
-        Stack<Integer> s = new Stack<>();
-        int current = 0, water = 0;
-        while(current < height.length) {
-            while(s.size() >= 1 && height[current] > height[s.peek()]) {
-                int barIdx = s.pop();
-                if(s.isEmpty()) break;
-                int dis = current - s.peek() -1;
-                int h = Math.min(height[s.peek()], height[current])-height[barIdx];
-                water += dis*h;
-            }
-            s.push(current++);
+        int n = height.length;
+        int[] maxleft = new int[n], maxright = new int[n];
+        
+        maxleft[0] = height[0];
+        for(int i = 1; i < n; i++) {
+            maxleft[i] = Math.max(maxleft[i-1], height[i]);
         }
-        return water;
+        
+        maxright[n-1] = height[n-1];
+        for(int i = n-2; i >= 0; i--) {
+            maxright[i] = Math.max(maxright[i+1], height[i]);
+        }
+        
+        int trappedwater = 0;
+        for(int i = 0; i < n; i++) {
+            trappedwater += Math.min(maxleft[i], maxright[i]) - height[i];
+        }
+        
+        return trappedwater;
     }
+    
+//     0,1,0,2,1,0,1,3,2,1,2,1
+        
+//     0 1 1 2 2 2 2 3 3 3 3 3
+//     3 3 3 3 3 3 3 3 2 2 2 1
+        
+//     0 0 1 0 1 2 1 0 0 1 0 0 += 6
 }
