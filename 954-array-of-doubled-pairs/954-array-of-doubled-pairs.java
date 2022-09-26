5d8699b7d107f9935767112daa1b5ae8ac02d8b1
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-09-26 23:22:23 +0530

    Time: 75 ms (59.97%), Space: 66.1 MB (81.05%) - LeetHub

--- 954-array-of-doubled-pairs/954-array-of-doubled-pairs.java
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean canReorderDoubled(int[] arr) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new TreeMap<>();
         for(int i: arr) {
             map.put(i, map.getOrDefault(i, 0)+1);
         }
         
-        ArrayList<Integer> a = new ArrayList(map.keySet());
-        
-        Collections.sort(a);
-        
-        for(int i: a) {
+        for(int i: map.keySet()) {
             if(map.get(i) != 0) {
                 int key = 2*i;
                 if(i < 0 && i%2 != 0) return false;
