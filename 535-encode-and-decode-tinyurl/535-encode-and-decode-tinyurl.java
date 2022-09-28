87de3b07b174376dee8a562e0e47b0764df4e277
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-09-28 13:21:29 +0530

    Time: 5 ms (61.76%), Space: 43.7 MB (34.26%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -1,13 +1,23 @@
 public class Codec {
 
+    HashMap<String, String> map;
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        return longUrl;
+        map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        char c = ((char)(Math.floor(Math.random()*100)));
+        sb.append(c);
+        while(map.containsKey(sb.toString())) {
+            c = ((char)(Math.floor(Math.random()*100)));
+            sb.append(c);
+        }
+        map.put(sb.toString(), longUrl);
+        return sb.toString();
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return shortUrl;
+        return map.get(shortUrl);
     }
 }
 
