8a2cdaa264f316b456af67702cc33d388ed01de4
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-10-18 23:52:10 +0530

    Time: 4 ms (85.69%), Space: 41.8 MB (71.17%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String countAndSay(int n) {
+        if(n <= 1) return "1";
+        
+        String s = countAndSay(n-1);
+        return countAndSay_(s);
+    }
+    
+    private String countAndSay_(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] arr = s.toCharArray();
+        
+        char prev = s.charAt(0);
+        int count = 1;
+        // for each
+        for(int i = 1; i < arr.length; i++) {
+            // same
+            if(prev == arr[i]) {
+                count++;
+            } 
+            // new
+            else {
+                sb.append(count);
+                sb.append(prev);
+                count = 1;
+                prev = arr[i];
+            }
+        }
+        sb.append(count);
+        sb.append(prev);
+        return sb.toString(); 
+    }
+}
