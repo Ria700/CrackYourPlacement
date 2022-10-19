5da35411af8b6e681f2290e45416953a26d7f717
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-10-19 17:16:52 +0530

    Time: 6 ms (96.26%), Space: 42.7 MB (89.23%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> cnt = new HashMap<>();
+        for (String word : words) {
+            cnt.put(word, cnt.getOrDefault(word, 0) + 1);
+        }
+        List<String> candidates = new ArrayList<>(cnt.keySet());
+        Collections.sort(candidates,
+                (w1, w2) -> cnt.get(w1).equals(cnt.get(w2)) ? w1.compareTo(w2) : cnt.get(w2) - cnt.get(w1));
+        return candidates.subList(0, k);
+    }
+}
