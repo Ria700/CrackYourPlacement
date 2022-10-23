c6e826594d3c30210c2dd85fbb6313b6fc946700
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-10-23 10:15:48 +0530

    Time: 10 ms (41.87%), Space: 54.9 MB (36.97%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] ans = new int[2];
+        for(int i = 0; i < nums.length; i++) {
+            int idx = Math.abs(nums[i])-1;
+            if(nums[idx] < 0) {
+                ans[0] = Math.abs(nums[i]);
+            } else nums[idx]*=-1;
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i]>0) ans[1] = i+1;
+        }
+        return ans;
+    }
+}
