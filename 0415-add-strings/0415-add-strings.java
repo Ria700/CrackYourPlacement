6e6ac16576d088bd21b4caaddffe70242e915f59
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-10-29 18:11:48 +0530

    Time: 3 ms (88.00%), Space: 43.5 MB (43.59%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,31 +1,14 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        int rem = 0;
+        int carry = 0;
         StringBuilder sb = new StringBuilder();
         int i = num1.length()-1, j = num2.length()-1;
-        while (i >= 0 && j >= 0) {
-            int num = rem + (num1.charAt(i--)-'0') + (num2.charAt(j--)-'0');
-            rem = num/10;
-            num%=10;
-            sb.insert(0, num);
+        while (i >= 0 || j >= 0 || carry == 1) {
+            int x = i < 0 ? 0 : num1.charAt(i--) - '0';
+            int y = j < 0 ? 0 : num2.charAt(j--) - '0';
+            sb.append((x + y + carry) % 10);
+            carry = (x + y + carry) / 10;
         }
-        while(i >= 0){
-            // sb.insert(0, num1.substring(0, i+1));
-            int num = rem + (num1.charAt(i--)-'0');
-            rem = num/10;
-            num%=10;
-            sb.insert(0, num);
-        }
-        while(j >= 0){
-            // sb.insert(0, num2.substring(0, j+1));
-            int num = rem + (num2.charAt(j--)-'0');
-            rem = num/10;
-            num%=10;
-            sb.insert(0, num);
-        }
-        if(rem > 0) {
-            sb.insert(0, rem);
-        }
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
