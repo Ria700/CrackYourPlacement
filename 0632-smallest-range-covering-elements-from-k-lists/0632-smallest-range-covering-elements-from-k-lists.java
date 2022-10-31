46d9cd927ec828bbda0b0b3b6bb5c21acbbed11e
Ria Monga <62502140+Ria700@users.noreply.github.com>
2022-10-31 23:43:12 +0530

    Time: 143 ms (18.62%), Space: 56.9 MB (43.58%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -7,7 +7,7 @@ class Solution {
         // max element - to calculate range
         // loop end - if pointer for any list exhausts
         // matching for next element 
-        int[] range = {-100000, 100000};
+        /* int[] range = {-100000, 100000};
         int k = nums.size();
         int max = Integer.MIN_VALUE;
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[0]-b[0]);
@@ -46,6 +46,35 @@ class Solution {
             tba[2] = idx+1;
             pq.add(tba);
         }
-        return range;
+        return range; */
+        
+        int minx = 0;
+        int miny = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int[] next = new int[nums.size()];
+        boolean flag = true;
+        
+        PriorityQueue <Integer> min_queue = new PriorityQueue <Integer> ((i, j) -> nums.get(i).get(next[i]) - nums.get(j).get(next[j]));
+        for (int i = 0; i < nums.size(); i++) {
+            min_queue.offer(i);
+            max = Math.max(max, nums.get(i).get(0));
+        }
+        for (int i = 0; i < nums.size() && flag; i++) {
+            for (int j = 0; j < nums.get(i).size() && flag; j++) {
+                int min_i = min_queue.poll();
+                if (miny - minx > max - nums.get(min_i).get(next[min_i])) {
+                    minx = nums.get(min_i).get(next[min_i]);
+                    miny = max;
+                }
+                next[min_i]++;
+                if (next[min_i] == nums.get(min_i).size()) {
+                    flag = false;
+                    break;
+                }
+                min_queue.offer(min_i);
+                max = Math.max(max, nums.get(min_i).get(next[min_i]));
+            }
+        }
+        return new int[] { minx, miny};
     }
 }
